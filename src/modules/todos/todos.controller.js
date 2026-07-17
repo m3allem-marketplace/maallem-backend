2ea8e750f61afc7dde48d2f2970cf589a5ba055b
Youssif Hamdy <youssifhamdy381@gmail.com>
2026-07-17 22:27:30 +0300

    fix: replace buggy todos controller (successResponse -> sendResponse)

--- src/modules/todos/todos.controller.js
+++ src/modules/todos/todos.controller.js
@@ -1,5 +1,5 @@
 const catchAsync = require("../../utils/catchAsync");
-const { successResponse } = require("../../utils/apiResponse");
+const { sendResponse } = require("../../utils/apiResponse");
 const todoService = require("./todos.service");
 
 class TodoController {
@@ -9,9 +9,7 @@ class TodoController {
 
     const todo = await todoService.createTodo(userId, todoData);
 
-    return res.status(201).json(
-      successResponse("Todo created successfully", { todo })
-    );
+    return sendResponse(res, 201, { todo }, "Todo created successfully");
   });
 
   getTodos = catchAsync(async (req, res) => {
@@ -22,9 +20,7 @@ class TodoController {
 
     const todos = await todoService.getTodos(userId, filters);
 
-    return res.status(200).json(
-      successResponse("Todos retrieved successfully", { todos })
-    );
+    return sendResponse(res, 200, { todos }, "Todos retrieved successfully");
   });
 
   getTodoById = catchAsync(async (req, res) => {
@@ -33,9 +29,7 @@ class TodoController {
 
     const todo = await todoService.getTodoById(userId, id);
 
-    return res.status(200).json(
-      successResponse("Todo retrieved successfully", { todo })
-    );
+    return sendResponse(res, 200, { todo }, "Todo retrieved successfully");
   });
 
   updateTodo = catchAsync(async (req, res) => {
@@ -45,9 +39,7 @@ class TodoController {
 
     const updatedTodo = await todoService.updateTodo(userId, id, updateData);
 
-    return res.status(200).json(
-      successResponse("Todo updated successfully", { todo: updatedTodo })
-    );
+    return sendResponse(res, 200, { todo: updatedTodo }, "Todo updated successfully");
   });
 
   deleteTodo = catchAsync(async (req, res) => {
@@ -56,9 +48,7 @@ class TodoController {
 
     await todoService.deleteTodo(userId, id);
 
-    return res.status(200).json(
-      successResponse("Todo deleted successfully")
-    );
+    return sendResponse(res, 200, null, "Todo deleted successfully");
   });
 }
 
